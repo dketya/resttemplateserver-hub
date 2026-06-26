239770f8a07dbe8301b4a78e7ca52c9fa1624172
dketya <d.ketya@gmail.com>
2026-06-26 14:53:03 +0700

    build commit

--- src/main/java/com/example/resttemplateserver/ResttemplateserverApplication.java
+++ src/main/java/com/example/resttemplateserver/ResttemplateserverApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResttemplateserverApplication {
+public class ResttemplateserverApplication { // This is the main class for the Spring Boot application
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResttemplateserverApplication.class, args);
